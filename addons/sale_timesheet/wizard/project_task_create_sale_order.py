cc222fefa34ef9f1d4e7f7f87a6ab7204261ec12
Victor Feyens <vfe@odoo.com>
2019-12-17 18:08:13 +0100

    [REF] sale: onchange --> compute stored not readonly

--- addons/sale_timesheet/wizard/project_task_create_sale_order.py
+++ addons/sale_timesheet/wizard/project_task_create_sale_order.py
@@ -66,8 +66,6 @@ class ProjectTaskCreateSalesOrder(models.TransientModel):
             'partner_id': self.partner_id.id,
             'analytic_account_id': self.task_id.project_id.analytic_account_id.id,
         })
-        sale_order.onchange_partner_id()
-        sale_order.onchange_partner_shipping_id()
 
         sale_order_line = self.env['sale.order.line'].create({
             'order_id': sale_order.id,
