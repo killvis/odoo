8f54cdbcfc2237206106bb25f8fd95a6514268c7
Jérémy Hennecart <jeh@odoo.com>
2019-12-18 12:43:08 +0100

    [IMP] mail: add author field on mail template
    When you configure a mail template, you can specify an author for the mail. This allows to simplify
    the setting of a template because you no longer need to now the format to select the email of the author.
    Example: for an invoice, you can set the author to "${object.user_id}" and the email_from field would be set to
    "${object.user_id.email_formatted |safe}".
    Additionaly, this allows when you send the mail to have the good author and not the user who clicks on the button send.
    
    task-2091859

--- addons/mail/models/mail_template.py
+++ addons/mail/models/mail_template.py
@@ -8,6 +8,7 @@ import datetime
 import dateutil.relativedelta as relativedelta
 import functools
 import logging
+import re
 
 from werkzeug import urls
 
@@ -105,7 +106,8 @@ class MailTemplate(models.Model):
                                     help="If checked, the user's signature will be appended to the text version "
                                          "of the message")
     subject = fields.Char('Subject', translate=True, help="Subject (placeholders may be used here)")
-    email_from = fields.Char('From',
+    author_id = fields.Char('Author', help="Author of the mail", placeholder="Author of the mail")
+    email_from = fields.Char('From', compute="_compute_email_from", inverse="_inverse_email_from", default=False,
                              help="Sender address (placeholders may be used here). If not set, the default "
                                   "value will be the author's email alias if configured, or email address.")
     use_default_to = fields.Boolean(
@@ -136,6 +138,32 @@ class MailTemplate(models.Model):
     auto_delete = fields.Boolean('Auto Delete', default=True, help="Permanently delete this email after sending it, to save space")
 
     scheduled_date = fields.Char('Scheduled Date', help="If set, the queue manager will send the email after the date. If not set, the email will be send as soon as possible. Jinja2 placeholders may be used.")
+
+    @api.depends('author_id')
+    def _compute_email_from(self):
+        for r in self:
+            if r.author_id:
+                email_from = r.author_id
+                pattern = r'(company_id[^\.])|(organizer_id)|(create_uid)|(user_id(\.id)?)|(partner_id(\(\))?)|([^\.]user\.)?(?(8)(id)?(email)?(( )?\|( )?safe)?|[^\.]user( ){0})'
+                matches = re.finditer(pattern, email_from)
+                done = []
+                for match in matches:
+                    if match.group() in done:
+                        continue                   
+                    email_from = re.sub(match.group(), match.group() + '.email_formatted', email_from)
+                    done.append(match.group())
+                r.email_from = email_from
+            else:
+                r.email_from = False
+
+    def _inverse_email_from(self):
+        for r in self:
+            if r.email_from:
+                pattern = re.compile(r'\.email_formatted(( )?\|( )?safe)?')
+                r.author_id = re.sub(pattern, '', r.email_from)
+            else:
+                r.author_id = False
+
 
     @api.onchange('model_id')
     def onchange_model_id(self):
@@ -345,7 +373,7 @@ class MailTemplate(models.Model):
             if template.lang:
                 Template = Template.with_context(lang=template._context.get('lang'))
             for field in fields:
-                Template = Template.with_context(safe=field in {'subject'})
+                Template = Template.with_context(safe=field in {'subject','email_from'})
                 generated_field_values = Template._render_template(
                     getattr(template, field), template.model, template_res_ids,
                     post_process=(field == 'body_html'))
@@ -357,6 +385,8 @@ class MailTemplate(models.Model):
             # update values for all res_ids
             for res_id in template_res_ids:
                 values = results[res_id]
+                if 'author_id' in fields and values.get('author_id'):
+                    values['author_id'] = self.env['res.users'].browse([int(s) for s in re.findall(r'\d+', values['author_id'])]).partner_id.id
                 # body: add user signature, sanitize
                 if 'body_html' in fields and template.user_signature:
                     signature = self.env.user.signature
@@ -430,7 +460,7 @@ class MailTemplate(models.Model):
         Attachment = self.env['ir.attachment']  # TDE FIXME: should remove default_type from context
 
         # create a mail_mail based on values, without attachments
-        values = self.generate_email(res_id, ['subject', 'body_html', 'email_from', 'email_to', 'partner_to', 'email_cc', 'reply_to', 'scheduled_date'])
+        values = self.generate_email(res_id, ['subject', 'body_html', 'author_id', 'email_from', 'email_to', 'partner_to', 'email_cc', 'reply_to', 'scheduled_date'])
         values['recipient_ids'] = [(4, pid) for pid in values.get('partner_ids', list())]
         values['attachment_ids'] = [(4, aid) for aid in values.get('attachment_ids', list())]
         values.update(email_values or {})
@@ -448,7 +478,7 @@ class MailTemplate(models.Model):
             else:
                 record = self.env[self.model].browse(res_id)
                 template_ctx = {
-                    'message': self.env['mail.message'].sudo().new(dict(body=values['body_html'], record_name=record.display_name)),
+                    'message': self.env['mail.message'].sudo().new(dict(body=values['body_html'], record_name=record.display_name, author_id=values['author_id'])),
                     'model_description': self.env['ir.model']._get(record._name).display_name,
                     'company': 'company_id' in record and record['company_id'] or self.env.company,
                     'record': record,
