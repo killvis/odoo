c357a919ccc97b6010c9451cad5fa32a096a1b65
Rémy Voet (ryv) <ryv@odoo.com>
2020-01-08 15:58:18 +0100

    [IMP] purchase_mrp: add link between MO/PO
    In case of MTO in some product used in a MO, Add the link between the MO
    source and the PO generated by.
    
    WIP : Doesn't work in pick pack shit (move_dest_ids -> ... )
    
    task-1913392

--- addons/purchase_mrp/models/purchase_mrp.py
+++ addons/purchase_mrp/models/purchase_mrp.py
@@ -1,15 +1,53 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-from odoo import api, fields, models
-from odoo.tools import float_compare
+from odoo import api, fields, models, _
 
 
 class MrpProduction(models.Model):
     _inherit = 'mrp.production'
 
+    mto_purchase_order_count = fields.Integer("Count PO", compute='_compute_mto_purchase_order_count')
+
+    @api.depends('move_raw_ids.created_purchase_line_id.order_id')
+    def _compute_mto_purchase_order_count(self):
+        for purchase in self:
+            purchase.mto_purchase_order_count = len(purchase.move_raw_ids.created_purchase_line_id.order_id)
+
     def _get_document_iterate_key(self, move_raw_id):
         return super(MrpProduction, self)._get_document_iterate_key(move_raw_id) or 'created_purchase_line_id'
+
+    def action_view_mto_mto_purchase_orders(self):
+        self.ensure_one()
+        return {
+            'name': _('Purchase Order generated from %s' % self.name),
+            'domain': [('id', 'in', self.move_raw_ids.created_purchase_line_id.order_id.ids)],
+            'res_model': 'purchase.order',
+            'type': 'ir.actions.act_window',
+            'view_mode': 'tree,form',
+        }
+
+
+class PurchaseOrder(models.Model):
+    _inherit = 'purchase.order'
+
+    mto_mrp_production_count = fields.Integer("Count MO Source", compute='_compute_mto_mrp_production_count')
+
+    @api.depends('order_line.move_dest_ids.raw_material_production_id')
+    def _compute_mto_mrp_production_count(self):
+        for purchase in self:
+            purchase.mto_mrp_production_count = len(purchase.order_line.move_dest_ids.raw_material_production_id)
+
+    def action_view_mto_mrp_productions(self):
+        self.ensure_one()
+        return {
+            'name': _('Manufacturing Source of %s' % self.name),
+            'domain': [('id', 'in', self.order_line.move_dest_ids.raw_material_production_id.ids)],
+            'res_model': 'mrp.production',
+            'type': 'ir.actions.act_window',
+            'view_mode': 'tree,form',
+        }
+
 
 class PurchaseOrderLine(models.Model):
     _inherit = 'purchase.order.line'
