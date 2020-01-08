268c8e166281ccbb91ba24ce769d742e89ea2a6c
mcm-odoo <mcm@odoo.com>
2020-01-08 09:47:56 +0100

    [REF] mail: migrate activity renderer to owl
    In order to prepare the future, we want to convert everything in Owl.
    This commit converts the activity renderer.
    
    task: 2149408

--- addons/mail/static/src/js/views/activity/activity_controller.js
+++ addons/mail/static/src/js/views/activity/activity_controller.js
@@ -1,16 +1,17 @@
 odoo.define('mail.ActivityController', function (require) {
 "use strict";
 
-var BasicController = require('web.BasicController');
-var core = require('web.core');
-var field_registry = require('web.field_registry');
-var ViewDialogs = require('web.view_dialogs');
+require('mail.Activity');
+const { BasicControllerAdapter } = require('web.ControllerAdapterMixin');
+const core = require('web.core');
+const field_registry = require('web.field_registry');
+const ViewDialogs = require('web.view_dialogs');
 
-var KanbanActivity = field_registry.get('kanban_activity');
-var _t = core._t;
+const KanbanActivityDate = field_registry.get('kanban_activity_date');
+const _t = core._t;
 
-var ActivityController = BasicController.extend({
-    custom_events: _.extend({}, BasicController.prototype.custom_events, {
+const ActivityController = BasicControllerAdapter.extend({
+    events: _.extend({}, BasicControllerAdapter.prototype.events, {
         empty_cell_clicked: '_onEmptyCell',
         send_mail_template: '_onSendMailTemplate',
         schedule_activity: '_onScheduleActivity',
@@ -20,46 +21,16 @@ var ActivityController = BasicController.extend({
     // Public
     //--------------------------------------------------------------------------
 
-    /**
-     * Overridden to remove the pager as it makes no sense in this view.
-     *
-     * @override
-     */
-    renderPager: function () {
-        return Promise.resolve();
-    },
-
     //--------------------------------------------------------------------------
     // Handlers
     //--------------------------------------------------------------------------
 
     /**
      * @private
-     */
-    _onScheduleActivity: function () {
-        var self = this;
-
-        var state = this.model.get(this.handle);
-        new ViewDialogs.SelectCreateDialog(this, {
-            res_model: state.model,
-            domain: this.model.originalDomain,
-            title: _.str.sprintf(_t("Search: %s"), this.renderer.arch.attrs.string),
-            no_create: !this.activeActions.create,
-            disable_multiple_selection: true,
-            context: state.context,
-            on_selected: function (record) {
-                var fakeRecord = self.renderer.getKanbanActivityData({}, record[0]);
-                var widget = new KanbanActivity(self, 'activity_ids', fakeRecord, {});
-                widget.scheduleActivity();
-            },
-        }).open();
-    },
-    /**
-     * @private
-     * @param {OdooEvent} ev
+     * @param {CustomEvent} ev
      */
     _onEmptyCell: function (ev) {
-        var state = this.model.get(this.handle);
+        const state = this.model.get(this.handle);
         this.do_action({
             type: 'ir.actions.act_window',
             res_model: 'mail.activity',
@@ -68,9 +39,9 @@ var ActivityController = BasicController.extend({
             views: [[false, 'form']],
             target: 'new',
             context: {
-                default_res_id: ev.data.resId,
+                default_res_id: ev.detail.resId,
                 default_res_model: state.model,
-                default_activity_type_id: ev.data.activityTypeId,
+                default_activity_type_id: ev.detail.activityTypeId,
             },
             res_id: false,
         }, {
@@ -79,17 +50,37 @@ var ActivityController = BasicController.extend({
     },
     /**
      * @private
-     * @param {OdooEvent} ev
+     */
+    _onScheduleActivity: function () {
+        const self = this;
+        const state = this.model.get(this.handle);
+        new ViewDialogs.SelectCreateDialog(this, {
+            res_model: state.model,
+            domain: this.model.originalDomain,
+            title: _.str.sprintf(_t("Search: %s"), this.renderer.props.arch.attrs.string),
+            no_create: !this.activeActions.create,
+            disable_multiple_selection: true,
+            context: state.context,
+            on_selected: function (record) {
+                const fakeRecord = state.getKanbanActivityData({}, record[0]);
+                const widget = new KanbanActivityDate(self, 'activity_ids', fakeRecord, {});
+                widget.scheduleActivity();
+            },
+        }).open();
+    },
+    /**
+     * @private
+     * @param {CustomEvent} ev
      */
     _onSendMailTemplate: function (ev) {
-        var templateID = ev.data.templateID;
-        var activityTypeID = ev.data.activityTypeID;
-        var state = this.model.get(this.handle);
-        var groupedActivities = state.grouped_activities;
-        var resIDS = [];
+        const templateID = ev.detail.templateID;
+        const activityTypeID = ev.detail.activityTypeID;
+        const state = this.model.get(this.handle);
+        const groupedActivities = state.grouped_activities;
+        const resIDS = [];
         Object.keys(groupedActivities).forEach(function (resID) {
-            var activityByType = groupedActivities[resID];
-            var activity = activityByType[activityTypeID];
+            const activityByType = groupedActivities[resID];
+            const activity = activityByType[activityTypeID];
             if (activity) {
                 resIDS.push(parseInt(resID));
             }
