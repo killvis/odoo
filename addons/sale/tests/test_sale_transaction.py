39f5661f0ef24d45de5f5909c0aade8cc313b4b3
Victor Feyens <vfe@odoo.com>
2020-01-09 17:31:48 +0100

    improve sale test

--- addons/sale/tests/test_sale_transaction.py
+++ addons/sale/tests/test_sale_transaction.py
@@ -22,8 +22,8 @@ class TestSaleTransaction(AccountTestCommon):
             ],
         })
         cls.env.ref('payment.payment_acquirer_transfer').journal_id = cls.cash_journal
-        if not cls.env.user.company_id.country_id:
-            cls.env.user.company_id.country_id = cls.env.ref('base.us')
+        if not cls.env.company.country_id:
+            cls.env.company.country_id = cls.env.ref('base.us')
 
         cls.transaction = cls.order._create_payment_transaction({
             'acquirer_id': cls.env.ref('payment.payment_acquirer_transfer').id,
@@ -37,16 +37,26 @@ class TestSaleTransaction(AccountTestCommon):
         - Create manually an invoice for this sale order.
         => The invoice must be paid.
         '''
+        taxes = self.order.order_line.tax_id.compute_all(
+                    100.0, self.order.currency_id,
+                    self.order.order_line.product_uom_qty, product=self.product, partner=self.order.partner_shipping_id)
+        taxes = sum(t.get('amount', 0.0) for t in taxes.get('taxes', []))
+        self.assertEqual(
+            self.order.amount_total,
+            sum(self.order.order_line.mapped('price_total')),
+        )
         self.transaction._set_transaction_done()
         self.transaction._post_process_after_done()
 
         # Assert a posted payment has been generated at this point.
         self.assertTrue(self.transaction.payment_id)
         self.assertEqual(self.transaction.payment_id.state, 'posted')
+        self.assertEquals(self.order, self.transaction.sale_order_ids)
 
         invoice = self.order._create_invoices()
         invoice.post()
 
+        self.assertIn(invoice, self.transaction.invoice_ids)
         self.assertEqual(invoice.invoice_payment_state, 'paid')
 
     def test_sale_transaction_mismatch(self):
